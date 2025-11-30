c0acfd5924b2a8ada594c808fd5b32e23e847299
malin-lb <malin.lagerblad@gmail.com>
2025-11-30 09:47:26 +0100

    Added secrets to repo

--- tests/logintests.test.ts
+++ tests/logintests.test.ts
@@ -1,10 +1,17 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 
+let password: string;
+
 test('consumer can log in successfully', async ({ page }) => {
     const login = new LoginPage(page);
     await login.goto();
-    await login.login('markus', 'sup3rs3cr3t', 'consumer');
+
+    if (process.env.PASSWORD !== undefined) {
+        password = process.env.PASSWORD;
+    }
+
+    await login.login('malin', password, 'consumer');
 
     await expect(page).toHaveURL(/\/store/i);
 });
